6a0492d56285775ba4cfec47708049f251c58c5a
dabadee <74227807+hassaan253@users.noreply.github.com>
2023-06-01 23:01:54 +0500

    Update main.m
    fixed a bug

--- main.m
+++ main.m
@@ -2,18 +2,15 @@
 
 %Choose cover format
 disp('T is for text. A is for Audio. I is for image');
-x = input( 'Choose cover format: ' );
-
-%Choose hidden message 
-y = input( 'Hidden message format :  ' );
-
+disp('We support encrypting text into images and audio, and images onto images');
+x = input( 'Choose cover format of either audio or image: ' ,'s');
 %text in audio
-if ( y == 'T' && x == 'A')
+    if (x == 'A')
 
     % ENCRYPTION   
     % Message to be embedded
 
-    message=input( 'Enter hidden message :  ' );
+    message=input( 'Enter hidden message :  ','s' );
     encrypted_text_in_audio = text_in_audio_encrypt(message);
 
     % DECRYPTION
@@ -21,12 +18,16 @@ if ( y == 'T' && x == 'A')
     % Number of characters of the hidden text
     chars =length(message);
     text_in_audio_decrypt(chars, encrypted_text_in_audio)
-    
+   
+    else 
+%Choose hidden message 
+y = input( 'Hidden message format of either text or image :  ','s');
+
 % text in image
-else if ( y == 'T' && x == 'I') 
+if ( y == 'T' && x == 'I') 
     
     % Message to be embedded
-    message=input( 'Enter hidden message :  ' );
+    message=input( 'Enter hidden message :  ' ,'s');
     
     % Number of characters of the hidden text
     chars =length(message);
@@ -36,10 +37,10 @@ else if ( y == 'T' && x == 'I')
     
     % DECRYPTION
     text_in_image_decrypt(chars, output)
-    
+   
 %image in image
-else if ( y == 'I' && x == 'I')
-        Message = input('Enter hidden image name (.png): ');
+elseif (y == 'I' && x == 'I')
+        Message = input('Enter hidden image name (.png): ','s');
         message = imread(Message);
         n = input('Choose a value for the number of bits (0 < n < 8): ');
         
@@ -48,7 +49,7 @@ else if ( y == 'I' && x == 'I')
         
         %DECRYPTION
         image_in_image_lsb_decrypt(n,Steganog_image);
-     
-    end
-    end
+else
+    disp('Invalid value')
 end
+    end 
